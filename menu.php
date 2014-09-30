8dea40ce6a9a1842f3c1efa0fad0507060a8bb68
Juan Salguero <jdsoccerplr@gmail.com>
2014-09-30 15:17:07 -0400

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,25 @@
+<?php echo '<ul id="menu">
+	<li><a href="http://jdsalguero.dx.am/contact/contact.html">Contact</a>
+	</li>
+	<li><a href="#">More...</a>
+		<ul>
+			<li><a href="http://jdsalguero.dx.am/movies/movies.html">Movie List</a></li>
+			<li><a href="http://jdsalguero.dx.am/pinterest/pinterest-d40bf.html" target="_blank">Pinterest</a></li>
+		</ul>
+	</li>
+	<li><a href="#">Social Network</a>
+		<ul>
+			<li><a href="https://www.facebook.com/juandiego.salgueroalcantara" target="_blank">Facebook</a></li>
+			<li><a href="https://www.linkedin.com/profile/view?id=241233077&trk=nav_responsive_tab_profile_pic" target="_blank">Linkedin</a></li>
+		</ul>
+	</li>
+	<li><a href="http://jdsalguero.dx.am/">About Me</a>
+		<ul>
+			<li><a href="http://jdsalguero.dx.am/Art%20Portfolio/art.html">Art Portfolio</a></li>
+			<li><a href="http://jdsalguero.dx.am/outdoor/outdoor.html">Outdoor Activities</a></li>
+			<li><a href="http://jdsalguero.dx.am/projects/projects.html">Projects</a></li>
+			<li><a href="http://jdsalguero.dx.am/resume/resume.html">Resume</a></li>
+		</ul>
+	</li>
+</ul>
+';?>
